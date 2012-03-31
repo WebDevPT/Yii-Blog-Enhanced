456a22e63fa73d0f1f61034f073f8d9336769303
WebDevPT <edgar.fernandes@gmail.com>
2012-03-31 07:45:37 +0100

    yii framework version added to about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -6,9 +6,9 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<p>This is the "about" page for my blog site.</p>
-
 <p>This website is based on the Yii Blog Demo project.</p>
+
+<p>Using Yii Framework v1.1.10</p>
 
 <p>The objective of this project is to enhance the initial demo version with new features.</p>
 

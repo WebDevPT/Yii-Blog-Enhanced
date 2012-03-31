cb6a76c9b8f96f8b3459a1e3a5e7da1e36fe17b9
WebDevPT <edgar.fernandes@gmail.com>
2012-03-31 06:35:15 +0100

    changed about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -9,3 +9,9 @@ $this->breadcrumbs=array(
 <p>This is the "about" page for my blog site.</p>
 
 <p>This website is based on the Yii Blog Demo project.</p>
+
+<p>The objective of this project is to enhance the initial demo version with new features.</p>
+
+<p>New Features List:</p>
+
+<p>soon</p>

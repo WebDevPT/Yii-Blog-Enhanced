bbe454c7bb002823f68432b82902dc42df91f896
WebDevPT <edgar.fernandes@gmail.com>
2012-03-31 06:26:29 +0100

    First commit

---
+++ protected/views/site/pages/about.php
@@ -0,0 +1,11 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - About';
+$this->breadcrumbs=array(
+	'About',
+);
+?>
+<h1>About</h1>
+
+<p>This is the "about" page for my blog site.</p>
+
+<p>This website is based on the Yii Blog Demo project.</p>

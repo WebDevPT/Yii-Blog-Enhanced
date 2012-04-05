699903d58f0504c3b0892b9119bd24a745059232
WebDevPT <edgar.fernandes@gmail.com>
2012-04-05 05:44:39 +0100

    updated about page and readme

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -10,19 +10,38 @@ $this->breadcrumbs=array(
 
 <p>Using Yii Framework v1.1.10</p>
 
-<p>The objective of this project is to enhance the initial demo version with new features.</p>
+<p>The objective of this project is to enhance the initial demo version with new features. Follow this project and you are welcome to contribute in any way. Join the forum discussion on Yii website.</p>
 
-<p>more info soon</p>
+<h4>Demo page</h4>
+<ul>
+    <li>soon</li>
+</ul>
+
+<h4>Forum discussion</h4>
+<ul>
+    <li><a href="http://www.yiiframework.com/forum/index.php/topic/30503-yii-blog-enhanced-on-github/">English</a></li>
+    <li><a href="http://www.yiiframework.com/forum/index.php/topic/30502-yii-blog-melhorado-no-github/">Portuguese</a></li>
+</ul>
+
+<h4>Github page</h4>
+<ul>
+    <li><a href="https://github.com/WebDevPT/Yii-Blog-Enhanced">Yii-Blog-Extended</a></li>
+</ul>
 
 <h4>New Features List:</h4>
 
 <ul>
-    <li>Wysiwyg Editor when creating new Post content using extension <a href="http://www.yiiframework.com/extension/xheditor-widget/">xheditor-widget</a>
-    </li> 
+    <li>Wysiwyg Editor when creating new Post content using extension <a href="http://www.yiiframework.com/extension/xheditor-widget">xheditor-widget</a></li>
+    <li>Search Bar using modified extension <a href="http://www.yiiframework.com/extension/esearch">esearch</a></li>
+    <li>Added Blog Statistics using extension <a href="http://www.yiiframework.com/extension/usercounter">usercounter</a></li>
+    <li>Added social bookmarks to posts using extension <a href="http://www.yiiframework.com/extension/wsocialbutton">wsocialbutton</a></li>
 </ul>
 
 <h4>Upcoming Features List:</h4>
 
 <ul>
-    <li>soon</li> 
+    <li>Custom made theme</li>
+    <li>User registration</li> 
+    <li>Banner Management</li> 
+    <li>more soon</li> 
 </ul>
